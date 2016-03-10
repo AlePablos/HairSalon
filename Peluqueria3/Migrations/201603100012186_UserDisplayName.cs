bc4855a645f61bfaf0b8fc815533dddecb755d63
Alejandro Pablos <alepablos03@gmail.com>
2016-03-09 21:24:08 -0300

    New migration to change User display header

---
+++ Peluqueria3/Migrations/201603100012186_UserDisplayName.cs
@@ -0,0 +1,16 @@
+namespace Peluqueria3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserDisplayName : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Peluqueria3/Models/User.cs
+++ Peluqueria3/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -11,29 +12,37 @@ namespace Peluqueria3.Models
         public int ID { get; set; }
 
         [StringLength(50)]
+        [DisplayName("First name")]
         public String firstName { get; set; }
 
         [StringLength (50)]
+        [DisplayName("Last name")]
         public String lastName { get; set; }
 
         [StringLength(15)]
+        [DisplayName("Phone")]
         public String phone { get; set; }
 
+        [DisplayName("Sex")]
         public bool sex { get; set; }  //1: male  0: female
 
         [Required(ErrorMessage = "The email address is required")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [DisplayName("Email")]
         public string email { get; set; }
 
         [Required(ErrorMessage = "The password is requered")]
         [DataType(DataType.Password)]
         [StringLength(50)]
+        [DisplayName("Password")]
         public string password { get; set; }
 
+        [DisplayName("Is Admin?")]
         public bool isAdmin { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayName("Last logged")]
         public DateTime lastLogged { get; set; }
 
     }
